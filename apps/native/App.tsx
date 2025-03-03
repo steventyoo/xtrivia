8afd513c94880c8c5c34111c64c1ccaa0360d708
Daniel Song <daniel124eng@gmail.com>
2025-03-03 12:27:44 -0500

    Fixed module unresolve issue for font file

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,12 +1,6 @@
-import { useFonts } from 'expo-font';
 import AppScreenRoot from './app/AppScreenRoot';
 
 export default function App() {
-
-  useFonts({
-    'TwemojiMozilla': require('@assets/fonts/TwemojiMozilla.woff2'),
-  });
-
   return (
     <AppScreenRoot />
   );
