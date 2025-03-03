463e293e2d9c899bc3e1391cdde21a1f3ae8be55
Daniel Song <daniel124eng@gmail.com>
2025-03-03 12:44:10 -0500

    Added gluestack (nativewind) for expo specialized.

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,6 +1,11 @@
 import AppScreenRoot from './app/AppScreenRoot';
+import "./global.css"
+import { verifyInstallation } from 'nativewind';
 
 export default function App() {
+
+  verifyInstallation()
+  
   return (
     <AppScreenRoot />
   );
